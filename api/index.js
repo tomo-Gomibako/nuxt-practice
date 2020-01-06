d59327e8c9a8d8d2d901539f13689694a793b23d
tomon9086 <tomo.mouse.9086@gmail.com>
2020-01-06 18:07:05 +0900

    :pensive:apiサーバーexpress化

--- api/index.js
+++ api/index.js
@@ -1,17 +1,12 @@
-const Koa = require('koa')
-const Router = require('koa-router')
+const express = require('express')
 
-const app = new Koa()
-const router = new Router()
+const app = express()
 
-router.get('/ping', (ctx) => {
-  ctx.body = 'pong'
+app.get('/ping', function (req, res) {
+  res.send('pong')
 })
-
-app.use(router.routes())
-app.use(router.allowedMethods())
 
 module.exports = {
   path: '/api/',
-  handler: app.callback()
+  handler: app
 }
